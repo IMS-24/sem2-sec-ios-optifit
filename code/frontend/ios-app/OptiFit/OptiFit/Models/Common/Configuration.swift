5f71aaa5ee03ab3149a7b62f669da44b3e21ce37
Markus stoegerer <markus.stoegerer@gmail.com>
2025-03-25 21:34:00 +0100

    extract b2c config

--- code/frontend/ios-app/OptiFit/OptiFit/Models/Common/Configuration.swift
+++ code/frontend/ios-app/OptiFit/OptiFit/Models/Common/Configuration.swift
@@ -1,11 +1,31 @@
 import Foundation
 
 enum Configuration {
+    static private func string(for key: String) -> String {
+        return Bundle.main.infoDictionary?[key] as! String
+    }
+
     static var apiBaseURL: URL {
         URL(string: string(for: "API_BASE_URL"))!
     }
 
-    static private func string(for key: String) -> String {
-        return Bundle.main.infoDictionary?[key] as! String
+    static var b2cClientId: String {
+        string(for: "B2C_CLIENT_ID")
+    }
+
+    static var b2cTenantId: String {
+        string(for: "B2C_TENANT")
+    }
+
+    static var b2cPolicyId: String {
+        string(for: "B2C_POLICY")
+    }
+
+    static var b2cAuthority: String {
+        string(for: "B2C_AUTHORITY")
+    }
+
+    static var b2cScopes: [String] {
+        string(for: "B2C_SCOPES").split(separator: ",").map(String.init)
     }
 }
--- code/frontend/ios-app/OptiFit/OptiFit/Services/AuthService.swift
+++ code/frontend/ios-app/OptiFit/OptiFit/Services/AuthService.swift
@@ -6,12 +6,12 @@ import MSAL
 @MainActor
 final class AuthService {
     // MARK: - Constants
-    private let tenantName = "optif1t.onmicrosoft.com"
-    private let authorityHostName = "optif1t.b2clogin.com"
-    private let clientID = "284ae6b3-8bd8-47cb-9ab1-98e74f4441db"
-    private let signupOrSigninPolicy = "b2c_1_susi"
+    private let tenantName = Configuration.b2cTenantId
+    private let authorityHostName = Configuration.b2cAuthority
+    private let clientID = Configuration.b2cClientId
+    private let signupOrSigninPolicy = Configuration.b2cPolicyId
     private let editProfilePolicy = "b2c_1_edit_profile"
-    private let scopes: [String] = ["https://optif1t.onmicrosoft.com/4e883ca0-1d29-4a61-ab49-d259b448b564/Profile.Read"]
+    private let scopes: [String] = Configuration.b2cScopes
     // URL format: https://<authorityHost>/tfp/<tenant>/<policy>
     private let endpointFormat = "https://%@/tfp/%@/%@"
     private let graphURI = "https://graph.microsoft.com"
