6946cfff3b59089b2ac9d12c40a79e9e50a2ffca
Markus stoegerer <markus.stoegerer@gmail.com>
2025-04-03 23:41:24 +0200

    update script 2

--- code/frontend/ios-app/OptiFit/OptiFit/update_version.sh
+++ code/frontend/ios-app/OptiFit/OptiFit/update_version.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Change working directory to the script's directory.
+cd "$(dirname "$0")" || exit
+
 # This script retrieves the current FullSemVer, Git branch, and Git hash from GitVersion,
 # then replaces the matching lines in multiple configuration files.
 # Each file should have lines starting with "GIT_HASH =", "FULL_SEMVER =", and "GIT_BRANCH =".
@@ -36,17 +39,10 @@ for TARGET_FILE in "${TARGET_FILES[@]}"; do
     continue
   fi
 
-  # Replace the entire line that starts with "GIT_HASH =" with the new ShortSha.
-  sed -i '' '/^GIT_HASH =/c\
-GIT_HASH = '"${SHORT_SHA}" "$TARGET_FILE"
-
-  # Replace the entire line that starts with "FULL_SEMVER =" with the new FullSemVer.
-  sed -i '' '/^FULL_SEMVER =/c\
-FULL_SEMVER = '"${FULL_SEMVER}" "$TARGET_FILE"
-
-  # Replace the entire line that starts with "GIT_BRANCH =" with the new branch name.
-  sed -i '' '/^GIT_BRANCH =/c\
-GIT_BRANCH = '"${BRANCH_NAME}" "$TARGET_FILE"
+  # Use substitution to replace the entire line.
+  sed -i '' -e "s/^GIT_HASH =.*/GIT_HASH = ${SHORT_SHA}/" "$TARGET_FILE"
+  sed -i '' -e "s/^FULL_SEMVER =.*/FULL_SEMVER = ${FULL_SEMVER}/" "$TARGET_FILE"
+  sed -i '' -e "s/^GIT_BRANCH =.*/GIT_BRANCH = ${BRANCH_NAME}/" "$TARGET_FILE"
 
   echo "Updated '$TARGET_FILE' with FullSemVer: ${FULL_SEMVER}, GIT_BRANCH: ${BRANCH_NAME}, and GIT_HASH: ${SHORT_SHA}"
 done
